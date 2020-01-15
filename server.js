9f6992fe94a28bf2ce7afaf0a753a22af1b0ca6a
kys0522 <kys0522@midasit.com>
2020-01-15 12:17:41 +0900

    monggodb Connect/Insert

--- server.js
+++ server.js
@@ -11,6 +11,62 @@ app.get('/', function(req, res) {
 });
 */
 
+// monggodb test
+var MongoClient = require('mongodb').MongoClient;
+
+var sampleDb = 'mongotest';
+var sampleCollection = 'testing';
+
+MongoClient.connect('mongodb://localhost:27017/mongotest', function(err, client) {
+    if(err) {
+        console.log('Fail to connect');
+        return
+    }
+    var db = client.db(sampleDb)
+    if(!db) console.log('Fail to use DB ' + sampleDb);
+
+    var collection = db.collection(sampleCollection);
+    if(!collection) console.log('Fail to use Collection' + sampleCollection);
+
+    console.log('Connected to mongodb!!');
+
+    var items = [ {
+        'title' : 'Snow Crash',
+        'author' : 'Neal Stephenson'
+    }, {
+        'title' : 'Snow Crash',
+        'author' : 'Neal Stephenson'
+    }, {
+        'title' : 'Hidden',
+        'author' : 'YS. Kim'
+    }];
+    
+    collection.insert(items, function(err, result) {
+    
+        if(err) {
+            console.log('Fail to Insert');
+            return;
+        }
+    
+        console.log(result.ops.length + ' record inserted.');
+        console.log(result.ops[0]._id + ' - ' + result.ops[0].title + ', ' + result.ops[0].author);
+    
+        collection.findOne({title:'Snow Crash'}, function(err, doc) {
+    
+            console.log(doc._id + ' - ' + doc.title + ', ' + doc.author);
+            client.close();
+        });
+    });
+});
+// test end
+
+/*
 var server = app.listen(app.get('port'), function() {
     console.log('Server up: http://localhost:' + app.get('port'));
 });
+*/
+
+
+
+
+    
