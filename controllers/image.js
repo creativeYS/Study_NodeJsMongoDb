1ee389cceb24deb4bababd7d661c51a8fa77eca7
kys0522 <kys0522@midasit.com>
2020-01-16 13:39:33 +0900

    * 댓글 기능 추가.

--- controllers/image.js
+++ controllers/image.js
@@ -2,6 +2,7 @@ var fs = require('fs'),
     path = require('path'),
     sidebar = require('../helpers/sidebar'),
     Models = require('../models');
+    md5 = require('MD5');
 
 module.exports = {
     index: function(req, res) {
@@ -13,7 +14,7 @@ module.exports = {
         };
 
         parameter:
-        Models.Image.findOne({filename:{ $regex:req.params.image_id}},
+        Models.Image.findOne({filename:{ $regex: req.params.image_id }},
             function(err, image) {
                 if(err) { throw err; }
 
@@ -37,44 +38,89 @@ module.exports = {
             });
     },
 
-    create: function(req, res) {
+    create: function (req, res) {
         //res.send('The image:create POST controller');
-        var saveImage = function() {
+
+        var saveImage = function () {
             var possible = 'abcdefghijklmnopqrstuvwxyz0123456789',
                 imgUrl = '';
 
-            for(var i=0; i < 6; i+=1) {
+            for (var i = 0; i < 6; i += 1) {
                 imgUrl += possible.charAt(Math.floor(Math.random() * possible.length));
             }
 
-            //var tempPath = req.files[0].file.path,
-            var tempPath = req.file.path,
-            //ext = path.extname(req.files[0].file.name).toLowerCase(),
-            ext = path.extname(req.file.originalname).toLowerCase(),
-            targetPath = path.resolve('./public/upload/' + imgUrl + ext);
+            Models.Image.find({ filename: imgUrl }, function (err, images) {
+                if (images.length > 0) {
+                    saveImage();
+                } else {
+                    //var tempPath = req.files[0].file.path,
+                    var tempPath = req.file.path,
+                        //ext = path.extname(req.files[0].file.name).toLowerCase(),
+                        ext = path.extname(req.file.originalname).toLowerCase(),
+                        targetPath = path.resolve('./public/upload/' + imgUrl + ext);
 
-            if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
-                fs.rename(tempPath, targetPath, function(err) {
-                    if (err) throw err;
+                    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+                        fs.rename(tempPath, targetPath, function (err) {
+                            if (err) throw err;
 
-                    res.redirect('/images/' + imgUrl);
-                });
-            } else {
-                fs.unlink(tempPath, function () {
-                    if (err) throw err;
+                            //res.redirect('/images/' + imgUrl);
+                            var newImg = new Models.Image( {
+                                title: req.body.title,
+                                description: req.body.description,
+                                filename: imgUrl + ext
+                            });
+                            newImg.save(function(err, image) {
+                                console.log('Successfully inserted image: '+image.filename);
+                                res.redirect('/images/'+image.uniqueId);
+                            })
+                        });
+                    } else {
+                        fs.unlink(tempPath, function () {
+                            if (err) throw err;
 
-                    res.json(500, {error: 'Only image files are allowed.'});
-                });
-            }
+                            res.json(500, { error: 'Only image files are allowed.' });
+                        });
+                    }
+                }
+            });
         };
 
         saveImage();
     },
     like: function(req, res) {
         //res.send('The image:like POST controller');
-        res.json({likes: 1});
+        Models.Image.findOne({filename: {$regex: req.params.image_id}},
+            function(err, image) {
+                if(!err && image) {
+                    image.likes = image.likes + 1;
+                    image.save(function(err) {
+                        if(err) {
+                            res.json(err);
+                        } else {
+                            res.json({likes:image.likes});
+                        }
+                    });
+                }
+            });
     },
     comment: function(req, res) {
-        res.send('The image:comment POST controller');
+        //res.send('The image:comment POST controller');
+
+        Models.Image.findOne({filename: {$regex: req.params.image_id}},
+            function(err, image) {
+                if(!err && image)
+                {
+                    var newComment = new Models.Comment(req.body);
+                    newComment.gravatar = md5(newComment.email);
+                    newComment.image_id = image._id;
+                    newComment.save(function(err, comment) {
+                        if(err) { throw errr;}
+
+                        res.redirect('/images/' + image.uniqueId + '#' + comment._id);
+                    });
+                } else {
+                    res.redirect('/');
+                }
+            });
     }
 };
--- server/configure.js
+++ server/configure.js
@@ -25,9 +25,14 @@ module.exports = function(app) {
     app.set('view engine', 'handlebars');
 
     app.use(morgan('dev'));
+
+    app.use(bodyParser.urlencoded({'extended':true}));
+    app.use(bodyParser.json());
+
     // app.use(bodyParser({
     //     uploadDir:path.join(__dirname, 'public/upload/temp')
     // }));
+
     app.use(multer({ 
         dest:path.join(__dirname, 'public/upload/temp')
     //}).any());
